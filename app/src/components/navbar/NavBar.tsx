43cea6cc4d28c0680e8ad867d248f9bcc11ec9ee
Moshir <alexandermoshir@yahoo.com>
2024-06-09 07:39:07 +0900

    Fixed Navbar to collapse on link press

--- app/src/components/navbar/NavBar.tsx
+++ app/src/components/navbar/NavBar.tsx
@@ -5,14 +5,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Carrot from '@assets/Carrot.svg';
-import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   const [theme, setTheme] = useState<string>(() => {
     const storedTheme = localStorage.getItem('theme');
     return storedTheme ? storedTheme : (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
   });
+
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   useEffect(() => {
     const handleThemeChange = () => {
@@ -36,10 +37,13 @@ const NavBar: React.FC = () => {
     document.documentElement.setAttribute('data-bs-theme', newTheme);
   };
 
+  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+  const handleOpenOffcanvas = () => setShowOffcanvas(true);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary mb-3">
       <Container fluid>
-        <Navbar.Brand href="/">
+        <Navbar.Brand as={Link} to="/">
           <img
             alt=""
             src={Carrot}
@@ -49,28 +53,30 @@ const NavBar: React.FC = () => {
           />{' '}
           Moshir.dev
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleOpenOffcanvas} />
         <Navbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
           placement="end"
+          show={showOffcanvas}
+          onHide={handleCloseOffcanvas}
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id="offcanvasNavbarLabel">
-              <Link to="/" className="text-decoration-none text-reset">
+              <Link to="/" className="text-decoration-none text-reset" onClick={handleCloseOffcanvas}>
                 Moshir.dev
               </Link>
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link as={NavLink} to="/blogs">Blogs</Nav.Link>
-              <Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
-              <Nav.Link as={NavLink} to="/games">Games</Nav.Link>
+              <Nav.Link as={NavLink} to="/blogs" onClick={handleCloseOffcanvas}>Blogs</Nav.Link>
+              <Nav.Link as={NavLink} to="/projects" onClick={handleCloseOffcanvas}>Projects</Nav.Link>
+              <Nav.Link as={NavLink} to="/games" onClick={handleCloseOffcanvas}>Games</Nav.Link>
               <Nav.Link href="https://github.com/MoshirMoshir/moshirmoshir.github.io" target="_blank" rel="noopener noreferrer">README.md</Nav.Link>
               <Nav.Item>
                 <button onClick={() => toggleTheme(theme === 'dark' ? 'light' : 'dark')} className="btn btn-outline-secondary ms-2">
-                {theme === 'dark' ? (
+                  {theme === 'dark' ? (
                     <i className="bi bi-sun-fill"></i> // Sun icon for dark mode
                   ) : (
                     <i className="bi bi-moon-fill"></i> // Moon icon for light mode
